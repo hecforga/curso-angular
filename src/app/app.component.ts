ae362f16b3a25e1395bbdfb610038e9f98cced30
Héctor Fornes <hector.fornes@nunsys.com>
2022-01-19 13:41:58 +0100

    Añadidas traducciones a PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Curso Angular';
+
+  constructor(private config: PrimeNGConfig) { }
+
+    ngOnInit() {
+        this.config.setTranslation({
+          monthNames: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+          emptyMessage: 'Sin resultados',
+          emptyFilterMessage: 'Sin resultados',
+        });
+    }
 }
