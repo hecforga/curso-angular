baa6910d3add0b4a9b4110c6d8c97698e39fbc77
Héctor Fornes <hector.fornes@nunsys.com>
2022-02-03 19:40:46 +0100

    Añadidos tests para DashboardComponent

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,50 +1,140 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
 
-import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { addMatchers, asyncData, click } from '../../testing';
 import { ProductService } from '../product.service';
 import { PRODUCTS } from '../mock-products';
 
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
 import { DashboardComponent } from './dashboard.component';
+import { AppModule } from '../app.module';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
-  let productService;
-  let getProductsSpy: jasmine.Spy;
+beforeEach(addMatchers);
 
+let comp: DashboardComponent;
+let fixture: ComponentFixture<DashboardComponent>;
+
+////////  Deep  ////////////////
+
+describe('DashboardComponent (deep)', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({imports: [AppModule]});
+  });
+
+  compileAndCreate();
+
+  tests(clickForDeep);
+
+  function clickForDeep() {
+    // get first <div class="product">
+    const productEl: HTMLElement = fixture.nativeElement.querySelector('.product');
+    click(productEl);
+  }
+});
+
+////////  Shallow ////////////////
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('DashboardComponent (shallow)', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule(
+        {declarations: [DashboardComponent], schemas: [NO_ERRORS_SCHEMA]});
+  });
+
+  compileAndCreate();
+
+  tests(clickForShallow);
+
+  function clickForShallow() {
+    // get first <product> DebugElement
+    const productDe = fixture.debugElement.query(By.css('.product'));
+    productDe.triggerEventHandler('click', comp.products[0]);
+  }
+});
+
+/** Add TestBed providers, compile, and create DashboardComponent */
+function compileAndCreate() {
   beforeEach(waitForAsync(() => {
-    productService = jasmine.createSpyObj('ProductService', ['getProducts']);
-    getProductsSpy = productService.getProducts.and.returnValue(of(PRODUCTS));
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const productServiceSpy = jasmine.createSpyObj('ProductService', ['getProducts']);
+
     TestBed
         .configureTestingModule({
-          declarations: [DashboardComponent, HeroSearchComponent],
-          imports: [RouterTestingModule.withRoutes([])],
-          providers: [{provide: ProductService, useValue: productService}]
+          providers: [
+            {provide: ProductService, useValue: productServiceSpy}, {provide: Router, useValue: routerSpy}
+          ]
         })
-        .compileComponents();
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(DashboardComponent);
+          comp = fixture.componentInstance;
+
+          // getProducts spy returns observable of test products
+          productServiceSpy.getProducts.and.returnValue(asyncData(PRODUCTS));
+        });
   }));
+}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+/**
+ * The (almost) same tests for both.
+ * Only change: the way that the first product is clicked
+ */
+function tests(productClick: () => void) {
+
+  it('should NOT have products before ngOnInit', () => {
+    expect(comp.products.length)
+      .withContext('should not have products before ngOnInit')
+      .toBe(0);
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should NOT have products immediately after ngOnInit', () => {
+    fixture.detectChanges();  // runs initial lifecycle hooks
+
+    expect(comp.products.length)
+      .withContext('should not have products until service promise resolves')
+      .toBe(0);
   });
 
-  it('should display "Top Coches" as headline', () => {
-    expect(fixture.nativeElement.querySelector('h2').textContent).toEqual('Top Coches');
+  describe('after get dashboard products', () => {
+    let router: Router;
+
+     // Trigger component so it gets products and binds to them
+    beforeEach(waitForAsync(() => {
+      router = fixture.debugElement.injector.get(Router);
+      fixture.detectChanges(); // runs ngOnInit -> getProducts
+      fixture.whenStable() // No need for the `lastPromise` hack!
+        .then(() => fixture.detectChanges()); // bind to products
+    }));
+
+    it('should HAVE products', () => {
+      expect(comp.products.length)
+        .withContext('should have products after service promise resolves')
+        .toBeGreaterThan(0);
+    });
+
+    it('should DISPLAY products', () => {
+      // Find and examine the displayed products
+      // Look for them in the DOM by css class
+      const products = fixture.nativeElement.querySelectorAll('.product');
+      expect(products.length)
+        .withContext('should display 4 products')
+        .toBe(4);
+    });
+
+    it('should tell ROUTER to navigate when product clicked', () => {
+      productClick();  // trigger click on first inner <div class="product">
+
+      // args passed to router.navigateByUrl() spy
+      const spy = router.navigateByUrl as jasmine.Spy;
+      const navArgs = spy.calls.first().args[0];
+
+      // expecting to navigate to id of the component's first product
+      const id = comp.products[0].id;
+      expect(navArgs)
+        .withContext('should nav to ProductDetail for first product')
+        .toBe('/products/detail/' + id);
+    });
   });
-
-  it('should call productService', waitForAsync(() => {
-       expect(getProductsSpy.calls.any()).toBe(true);
-     }));
-
-  it('should display 4 links', waitForAsync(() => {
-       expect(fixture.nativeElement.querySelectorAll('a').length).toEqual(4);
-     }));
-});
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
@@ -10,13 +12,18 @@ import { ProductService } from '../product.service';
 export class DashboardComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts(): void {
+  goToDetail(product: Product) {
+    const url = `/products/detail/${product.id}`;
+    this.router.navigateByUrl(url);
+  }
+
+  private getProducts(): void {
     this.productService.getProducts()
       .subscribe(products => this.products = products.slice(1, 5));
   }
